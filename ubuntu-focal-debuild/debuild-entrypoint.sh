341ae65f3b56fb5dc52af9462dc855bf0de3d3ca
Laurent Boulard <laurent.boulard@gmail.com>
2020-05-07 01:40:48 +0200

    Add Ubuntu Eoan and Uuntu Focal releases

---
+++ ubuntu-focal-debuild/debuild-entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+set -e
+
+if [ -n "$UID" -a -n "$GID" ]; then
+	groupadd -g $GID guest
+	useradd -r -u $UID -g $GID guest
+elif [ -n "$UID" ]; then
+	useradd -r -u $UID guest
+fi
+
+# behave as init 1 to handle signal and wait process
+trap 'kill $pid; wait $pid; exit 1' INT TERM QUIT
+
+apt-get update &
+pid=$!
+wait $pid
+[ -f debian/control ] && {
+	(export DEBIAN_FRONTEND=noninteractive; yes | mk-build-deps -i -r) &
+	pid=$!
+	wait $pid
+}
+
+[ -n "$CCACHE_DIR" ] && {
+	[ -d "$CCACHE_DIR" ] || mkdir -p "$CCACHE_DIR" || exit 1
+	chown -R ${UID:-$(id -u)}:${GID:-$(id -g)} "$CCACHE_DIR" &
+	pid=$!
+	wait $pid
+}
+pid=
+set -x
+exec /usr/bin/tini -- ${UID:+gosu guest} \
+	eatmydata debuild \
+	${CCACHE_DIR:+--preserve-envvar=CCACHE_DIR --prepend-path=/usr/lib/ccache} \
+	-I.git -i'\.git/' \
+	-us -uc \
+	$@
